5883f50b3d9c90ec6f7a80fad8766b4aaee52414
Hayden Carr <HaydenCarrBusiness@gmail.com>
2024-12-06 14:39:50 +0000

    fixed commenting

--- serial/update_velocity.c
+++ serial/update_velocity.c
@@ -7,13 +7,13 @@ void update_velocity(BODY *bodies, double delta_time)
     {
         printf("Body %d Velocity x is: %f, and y is: %f\n", i, bodies[i].vx, bodies[i].vy);
         // compute the acceleration in x and y dir
-        double ax = bodies[i].fx / bodies[i].mass; // Acceleration in x direction
-        double ay = bodies[i].fy / bodies[i].mass; // Acceleration in y direction
+        double ax = bodies[i].fx / bodies[i].mass; // acceleration in x direction
+        double ay = bodies[i].fy / bodies[i].mass; // acceleration in y direction
         ax = ax * delta_time;
         ay = ay * delta_time;
 
         // update the velocity of body n
-        bodies[i].vx += ax; // New velocity in x direction
-        bodies[i].vy += ay; // New velocity in y direction
+        bodies[i].vx += ax; // new velocity in x direction
+        bodies[i].vy += ay; // new velocity in y direction
     }
 }
